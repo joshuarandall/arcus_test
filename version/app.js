76964b8509ccebbb352c1f16ed926f1cf0a55389
joshuarandall <67920841+joshuarandall@users.noreply.github.com>
2020-09-22 22:30:13 -0500

    Go Tim GO

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 01 of the app.' + '\n';
+    response = 'This is version 01 of the app. Arcus soon to come, go Tim GO!' + '\n';
 
     //send the response to the client
     res.send(response);
